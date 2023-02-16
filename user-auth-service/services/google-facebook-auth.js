7344e0b6fc97d6cc4b816100f1c49d70639ad1c3
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-02-16 15:22:39 -0500

    Updated email auth

--- user-auth-service/services/google-facebook-auth.js
+++ user-auth-service/services/google-facebook-auth.js
@@ -1,7 +1,3 @@
-/*
- * User auth passport strategy for Facebook and Gmail
- */
-
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
@@ -9,72 +5,72 @@ const mongoose = require("mongoose");
 const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+    done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => {
-    done(null, user);
-  });
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
 });
 
 // Google Auth
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+    new GoogleStrategy({
+        clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_AUTH_SECRET,
+        callbackURL: "/api/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(profile);
+     
       // check if user id already exists
-      User.findOne({ userId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
+        User.findOne({ userId: profile.id }).then((existingUser) => {
+            if (existingUser) {
+            done(null, existingUser);
+            } else {
           // adding new user
-          new User({
-            userId: profile.id,
-            status: "Active",
-          })
-            .save()
-            .then((user) => {
-              done(null, user);
-            });
-        }
-      });
+                new User({
+                    userId: profile.id,
+                    status: "Active",
+                })
+                .save()
+                .then((user) => {
+                    done(null, user);
+                });
+            }
+        });
     }
   )
 );
 
 // Facebook Auth
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-      callbackURL: "/api/auth/facebook/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(profile);
-      // check if user id already exists
-      User.findOne({ userId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          // adding new user
-          console.log("Adding a new user");
-          new User({
-            userId: profile.id,
-            status: "Active",
-          })
-            .save()
-            .then((user) => {
-              done(null, user);
-            });
-        }
-      });
-    }
-  )
-);
+// passport.use(
+//   new FacebookStrategy(
+//     {
+//       clientID: process.env.FACEBOOK_CLIENT_ID,
+//       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+//       callbackURL: "/api/auth/facebook/callback",
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       console.log(profile);
+//       // check if user id already exists
+//       User.findOne({ userId: profile.id }).then((existingUser) => {
+//         if (existingUser) {
+//           done(null, existingUser);
+//         } else {
+//           // adding new user
+//           console.log("Adding a new user");
+//           new User({
+//             userId: profile.id,
+//             status: "Active",
+//           })
+//             .save()
+//             .then((user) => {
+//               done(null, user);
+//             });
+//         }
+//       });
+//     }
+//   )
+// );
+
--- user-auth-service/services/nodemailer-service.js
+++ user-auth-service/services/nodemailer-service.js
@@ -1,34 +1,39 @@
-/*
- * Service to send confirmation email for registration verification
- */
 const nodemailer = require("nodemailer");
-
-// Configuring sender info for sending confirmation emails
-const transport = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: process.env.user,
-    pass: process.env.password,
-  },
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        type: 'OAuth2',
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASSWORD,
+        tls: {
+            rejectUnauthorized: false
+        },
+        clientId: process.env.GMAIL_CLIENT_ID,
+        clientSecret: process.env.GMAIL_CLIENT_SECRET,
+        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+        accessToken: process.env.GMAIL_ACCESS_TOKEN 
+    },
 });
 
-module.exports.sendConfirmationEmail = (email, confirmationCode) => {
-  console.log("Sending email to ", email);
-  transport
-    .sendMail({
-      from: process.env.user,
-      to: email,
-      subject: "Please confirm your email",
-      html: `<div><img src="https://res.cloudinary.com/e-bechmanis/image/upload/v1671245485/Group63_zlw4bt.png" alt="logo" style="display:inline-block;width:225px;margin-bottom:2rem">
+module.exports.sendConfirmationEmail = async(email, confirmationCode) => {
+    console.log("user ", process.env.GMAIL_USER)
+    const info = await transporter
+        .sendMail({
+            from: process.env.GMAIL_USER,    
+            to: email,
+            subject: "Please confirm your email",
+            html: `<div><img src="https://res.cloudinary.com/e-bechmanis/image/upload/v1671245485/Group63_zlw4bt.png" alt="logo" style="display:inline-block;width:225px;margin-bottom:2rem">
              <div style="font-family:sans-serif;font-size:14px;line-height:22px"><h2 style="color:#F46B5D;margin-bottom:3rem">Confirm email</h2>
                 <div style="max-width:421px"><p>Hello, dear!</p>
                     <p>Thank you for registering with WeFriiends!<br/>To confirm your e-mail address, please click the button below.</p><br/>
-                    <a href=http://localhost:8080/api/auth/confirm/${confirmationCode} style="display:flex;align-items:center;width:288px;justify-content:center;text-align:center;padding:18px 24px;gap:16px;background:#FB8F67;border-radius:10px;text-decoration:none;color:white;font-size:18px;font-weight:600"> confirm email</a>
+                    <a href=https://clumsy-glasses-clam.cyclic.app/api/auth/confirm/${confirmationCode} style="display:flex;align-items:center;width:288px;justify-content:center;text-align:center;padding:18px 24px;gap:16px;background:#FB8F67;border-radius:10px;text-decoration:none;color:white;font-size:18px;font-weight:600"> Confirm email</a>
                     <br/><br/>
                     <p>Still have questions? Head over to our comprehensive <a href="https://wefriiends.atlassian.net/servicedesk/customer/portals" style="color:#F46B5D">Help Center<a/> or just reach out to our <a href="https://wefriiends.atlassian.net/servicedesk/customer/portals" style="color:#F46B5D">support team.</a></p>
                     <p>We look forward to having you on board.</p>
                     <p>Kind regards,<br/>The WeFriiends Team</p><div/></div>
-              <div style="color:#F46B5D;position:absolute;width:100%;height:120px;padding-top:1rem;background-color:#FFF1EC">www.wefriiends.com<br/><br/><a href="mailto:info@wefriiends.com" style="color:#F46B5D">contact us</a>&nbsp; &nbsp; &nbsp; &nbsp; unsubscribe</div></div>`,
-    })
-    .catch((err) => console.log(err));
+                    <div style="color:#F46B5D;position:absolute;width:100%;height:120px;padding-top:1rem;background-color:#FFF1EC">www.wefriiends.com<br/><br/><a href="mailto:info@wefriiends.com" style="color:#F46B5D">contact us</a>&nbsp; &nbsp; &nbsp; &nbsp; unsubscribe</div></div>`,
+        })
+        if (!info.messageId) {
+            console.log("Something went wrong while sending the confirmation email")
+        }
 };

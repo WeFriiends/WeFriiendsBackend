86b685bac37c1227ef45aa38f3509794ff984c24
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2023-12-29 01:20:54 -0500

    Update date.js

--- user-profile-service/services/date.js
+++ user-profile-service/services/date.js
@@ -5,23 +5,15 @@ let Profile = mongoose.model("profiles");
 // This function adds date of birth and zodiac sign
 // dob param should have format "<YYYY-mm-dd>"
 module.exports.addDateOfBirthAndZodiac = (id, dob) => {
-  return new Promise((resolve, reject) => {
-    console.log("got here")
+  return new Promise((resolve, reject) => {  
     Profile.find({
       userId: id,
     })
       .exec()
-      .then((profile) => {
-        console.log(dob);
+      .then((profile) => {       
         let date = parseInt(dob.slice(9, 11));
-        console.log(date);
-        console.log(typeof date);
-
         let month = parseInt(dob.slice(6, 8));
-        console.log(month);
-        console.log(typeof month);
         let userZodiac = zodiac.getSignByDate({ day: date, month: month });
-        console.log(userZodiac);
         Profile.findOneAndUpdate(
           { userId: id },
           { dob: new Date(dob), zodiacSign: userZodiac.name },
